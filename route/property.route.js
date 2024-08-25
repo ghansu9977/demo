67248c95e0d5426733fb3000d67346e96a6d8c11
ghansu9977 <ghanshyamkushwah623@gmail.com>
2024-08-25 00:26:53 -0700

    short correction

--- route/property.route.js
+++ route/property.route.js
@@ -6,10 +6,10 @@ const router = express.Router();
 
 router.post("/createProperty", createProperty);
 
-router.post("/add-property", addProperty);
+router.post("/add-property",auth,addProperty);
 router.get("/viewProperties", getAllProperties);
 router.get("/search", auth, searchProperties);
-router.post("/favorite/:id",  markAsFavorite);
+router.post("/favorite/:id",auth,markAsFavorite);
 router.get("/viewFavourites",auth, getFavorites);
 router.delete("/deleteProperty/:id", auth, deleteProperty);     
 router.post("/add-property-in-bulk",createMultipleProperties);
